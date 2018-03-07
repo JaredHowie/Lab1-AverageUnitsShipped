5efcf4e0f0cd88c51f2e5dd6e4904c2115b5a63f
Jared <Jared@DESKTOP-Q38CSK3>
2018-03-07 11:04:24 -0500

    Add project files.

---
+++ Lab1-AverageUnitsShipped/.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAverageUnitsShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblUnits = New System.Windows.Forms.Label()
+        Me.tbUnitsShipped = New System.Windows.Forms.TextBox()
+        Me.txtDay = New System.Windows.Forms.TextBox()
+        Me.tbAllUnitsShipped = New System.Windows.Forms.TextBox()
+        Me.tbAverageUnitsShipped = New System.Windows.Forms.TextBox()
+        Me.txtDayNumber = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnEnter
+        '
+        Me.btnEnter.AccessibleName = "btnEnter"
+        Me.btnEnter.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnEnter.Location = New System.Drawing.Point(13, 218)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 2
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.AccessibleName = "btnReset"
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(104, 218)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 3
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.AccessibleName = "btnExit"
+        Me.btnExit.Location = New System.Drawing.Point(195, 218)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblUnits
+        '
+        Me.lblUnits.AccessibleName = "lblUnits"
+        Me.lblUnits.AutoSize = True
+        Me.lblUnits.Location = New System.Drawing.Point(43, 23)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(44, 17)
+        Me.lblUnits.TabIndex = 3
+        Me.lblUnits.Text = "Units:"
+        '
+        'tbUnitsShipped
+        '
+        Me.tbUnitsShipped.AccessibleName = "tbUnitsShipped"
+        Me.tbUnitsShipped.Location = New System.Drawing.Point(104, 23)
+        Me.tbUnitsShipped.Name = "tbUnitsShipped"
+        Me.tbUnitsShipped.Size = New System.Drawing.Size(75, 22)
+        Me.tbUnitsShipped.TabIndex = 1
+        '
+        'txtDay
+        '
+        Me.txtDay.AccessibleName = "txtDay"
+        Me.txtDay.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtDay.Enabled = False
+        Me.txtDay.Location = New System.Drawing.Point(185, 23)
+        Me.txtDay.Name = "txtDay"
+        Me.txtDay.Size = New System.Drawing.Size(35, 15)
+        Me.txtDay.TabIndex = 0
+        Me.txtDay.Text = "Day "
+        '
+        'tbAllUnitsShipped
+        '
+        Me.tbAllUnitsShipped.AccessibleName = "txtAllUnitsShipped"
+        Me.tbAllUnitsShipped.Enabled = False
+        Me.tbAllUnitsShipped.Location = New System.Drawing.Point(46, 51)
+        Me.tbAllUnitsShipped.MaximumSize = New System.Drawing.Size(4, 200)
+        Me.tbAllUnitsShipped.MinimumSize = New System.Drawing.Size(192, 130)
+        Me.tbAllUnitsShipped.Multiline = True
+        Me.tbAllUnitsShipped.Name = "tbAllUnitsShipped"
+        Me.tbAllUnitsShipped.Size = New System.Drawing.Size(192, 133)
+        Me.tbAllUnitsShipped.TabIndex = 6
+        '
+        'tbAverageUnitsShipped
+        '
+        Me.tbAverageUnitsShipped.AccessibleName = "tbAverageUnitsShipped"
+        Me.tbAverageUnitsShipped.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.tbAverageUnitsShipped.Enabled = False
+        Me.tbAverageUnitsShipped.Location = New System.Drawing.Point(46, 190)
+        Me.tbAverageUnitsShipped.Name = "tbAverageUnitsShipped"
+        Me.tbAverageUnitsShipped.Size = New System.Drawing.Size(192, 22)
+        Me.tbAverageUnitsShipped.TabIndex = 7
+        '
+        'txtDayNumber
+        '
+        Me.txtDayNumber.AccessibleName = "txtDayNumber"
+        Me.txtDayNumber.BackColor = System.Drawing.SystemColors.Control
+        Me.txtDayNumber.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtDayNumber.Location = New System.Drawing.Point(220, 23)
+        Me.txtDayNumber.Name = "txtDayNumber"
+        Me.txtDayNumber.Size = New System.Drawing.Size(18, 15)
+        Me.txtDayNumber.TabIndex = 8
+        '
+        'frmAverageUnitsShipped
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AccessibleName = "frmAverageUnitsShipped"
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.Controls.Add(Me.txtDayNumber)
+        Me.Controls.Add(Me.tbAverageUnitsShipped)
+        Me.Controls.Add(Me.tbAllUnitsShipped)
+        Me.Controls.Add(Me.txtDay)
+        Me.Controls.Add(Me.tbUnitsShipped)
+        Me.Controls.Add(Me.lblUnits)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmAverageUnitsShipped"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblUnits As Label
+    Friend WithEvents tbUnitsShipped As TextBox
+    Friend WithEvents txtDay As TextBox
+    Friend WithEvents tbAllUnitsShipped As TextBox
+    Friend WithEvents tbAverageUnitsShipped As TextBox
+    Friend WithEvents txtDayNumber As TextBox
+End Class
---
+++ Lab1-AverageUnitsShipped/AverageUnitsShipped.vb
@@ -0,0 +1,92 @@
+﻿Public Class frmAverageUnitsShipped
+    Private dayNumber As Integer = 1
+    Private allUnitsShipped(7) As Integer
+    Private averageShipped As Double = 0.0
+
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        'Clesr all text boxes as well as resets all form level variables
+        tbAllUnitsShipped.Clear()
+        tbAverageUnitsShipped.Clear()
+        txtDay.Clear()
+        dayNumber = 1
+        tbUnitsShipped.Clear()
+        txtDay.Text() = "Day"
+        dayNumber = 1
+        txtDayNumber.Text() = dayNumber.ToString
+        btnEnter.Enabled() = True
+        tbUnitsShipped.Enabled() = True
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'close the form when the exit button is clicked
+        Close()
+
+
+    End Sub
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        'Create the variables needed
+        Dim userInput As String = tbUnitsShipped.Text()
+        Dim validUserInput As Integer = 0
+        Dim sumUnitsShipped As Integer = 0
+        'if we have not finished puting numbers in for the week
+        If dayNumber <= 7 Then
+            txtDayNumber.Text() = dayNumber.ToString
+            'if the text entered is a whole number
+            If Integer.TryParse(userInput, validUserInput) Then
+                'if the number entered is within the range
+                If validUserInput < 1000 And validUserInput > 0 Then
+                    'display the number in the textbox on a new line
+                    tbAllUnitsShipped.Text() = tbAllUnitsShipped.Text() + tbUnitsShipped.Text() + vbNewLine
+                    'put the valid number in the area 
+                    allUnitsShipped(dayNumber - 1) = validUserInput
+                    tbUnitsShipped.Select()
+                    'move the day forward
+                    dayNumber = dayNumber + 1
+
+                Else
+                    'warn the user that the number they entred is not in the range
+                    MessageBox.Show("The number entered must be between 0 and 1000 inclusive")
+                    tbUnitsShipped.Select()
+                End If
+            Else
+                'warn the user that they did not enter a whole number
+                MessageBox.Show("You must enter a whole number!")
+                tbUnitsShipped.Select()
+            End If
+        Else
+            'disable the enter button
+            btnEnter.Enabled() = False
+            'disable the text box where the user entres the data
+            tbUnitsShipped.Enabled() = False
+            'loop through the array and add the sum of all the days up
+            For counter As Integer = 0 To 6
+                sumUnitsShipped = sumUnitsShipped + allUnitsShipped(counter)
+            Next
+            'find the average using the calculated sum
+            averageShipped = sumUnitsShipped / 7
+            'round the calculated average to 2 decimal places
+            averageShipped = Math.Round(averageShipped, 2)
+            'display the calculated average to the user
+            tbAverageUnitsShipped.Text() = "Average per day: " + averageShipped.ToString
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub txtDay_TextChanged(sender As Object, e As EventArgs) Handles txtDay.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtDayNumber.TextChanged
+
+    End Sub
+
+    Private Sub frmAverageUnitsShipped_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtDayNumber.Text() = dayNumber.ToString
+    End Sub
+End Class
